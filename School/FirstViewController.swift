8359babff6ab898738a2582e18d1c40f31cf33f6
theSatellite101 <tdandachi@tareqs-macbook-air.local>
2017-05-11 21:32:05 +0400

    Initial Commit

---
+++ School/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  School
+//
+//  Created by Tareq El Dandachi on 5/11/17.
+//  Copyright © 2017 Tareq El Dandachi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ School/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  School
+//
+//  Created by Tareq El Dandachi on 5/11/17.
+//  Copyright © 2017 Tareq El Dandachi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
